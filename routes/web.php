e828db1576f8ae4923d821a0c2f5bda95ca939cf
TALEX1995 <tanase.alex@libero.it>
2023-08-25 15:29:17 +0200

    Created home and comics page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ComicController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
+// Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
+// Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
+// Route::get('/comics/store', [ComicController::class, 'store'])->name('comics.store');
