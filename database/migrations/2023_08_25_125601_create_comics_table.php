be87f52a7bd521c5d05bb245d9243a73fd95f64a
TALEX1995 <tanase.alex@libero.it>
2023-08-27 13:04:35 +0200

    Changed migrate comic descritption in text

--- database/migrations/2023_08_25_125601_create_comics_table.php
+++ database/migrations/2023_08_25_125601_create_comics_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
             $table->string('title')->unique();
-            $table->string('description');
+            $table->text('description');
             $table->float('price', 6, 2);
             $table->string('series');
             $table->date('sale_date');
