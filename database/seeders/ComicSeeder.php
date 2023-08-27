06f82a822edb6051bc58bc2ee5f79df61f8a9dc6
TALEX1995 <tanase.alex@libero.it>
2023-08-27 13:00:51 +0200

    Added seeder for comics

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,25 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = config('comics');
+
+        foreach ($data as $d) {
+            $comic = new Comic();
+
+            $comic->title = $d['title'];
+            $comic->description = $d['description'];
+            $comic->price = $d['price'];
+            $comic->series = $d['series'];
+            $comic->sale_date = $d['sale_date'];
+            $comic->type = $d['type'];
+            // Transform array artists in string
+            $artistsString = implode(',', $d['artists']);
+            $comic->artists = $artistsString;
+            // Transform array writers in string
+            $writersString = implode(',', $d['writers']);
+            $comic->writers = $writersString;
+
+            $comic->save();
+        }
     }
 }
