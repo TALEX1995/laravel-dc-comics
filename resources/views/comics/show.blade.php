236e1b3c0b515a70addf2ed38b29819c2b47ae9e
TALEX1995 <tanase.alex@libero.it>
2023-08-29 15:48:55 +0200

    Added SoftDelete Feature

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,6 +2,15 @@
 
 
 @section('main-content')
-  <h1>{{ $comic->title }}</h1>
-  <a href="{{ route('comics.index') }}">Torna ai fumetti</a>
+  <div class="container">
+    <h1>{{ $comic->title }}</h1>
+    <div class="d-flex justify-content-between">
+      <a href="{{ route('comics.index') }}">Torna ai fumetti</a>
+      <form method="POST" action="{{ route('comics.destroy', $comic) }}">
+        @csrf
+        @method('DELETE')
+        <button class="btn btn-danger">Elimina</button>
+      </form>
+    </div>
+  </div>
 @endsection
